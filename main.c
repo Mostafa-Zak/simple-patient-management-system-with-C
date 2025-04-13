29af1a3e6dda38dd0083d938498fdf5befcfadf4
Mostafa-Zak <mostafazakzok21@gmail.com>
2025-04-13 18:15:17 +0200

    making op dynamic

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,27 +8,51 @@ typedef struct operation
     char name[50];
     char stage[50];
 }op;
+
+typedef struct
+{
+    op *array;
+    size_t used;
+    size_t size;
+} opArray;
+
 struct patient 
 {
     char name[50];
     bool status;
-    op op;
+    opArray ops;
 };
-
 typedef struct 
 {
     struct patient *array;
     size_t used;
     size_t size;
-} Array;
+} pArray;
 
-void initArray(Array *a,size_t initialSize)
+void initpArray(pArray *a,size_t initialSize)
 {
     a -> array = malloc(initialSize * sizeof(struct patient));
     a -> used = 0;
     a ->size = initialSize;
 }
-void insertArray(Array *a, struct patient element)
+void initOpArray(opArray *a, size_t initialSize)
+{
+    a->array = malloc(initialSize * sizeof(op));
+    a-> used = 0;
+    a->size = initialSize;
+};
+
+void insertOpArray(opArray *a, op element)
+{
+    if (a->used == a->size)
+    {
+	a->size *=2;
+	a->array = realloc(a->array, a->size * sizeof(op));
+    }
+    a->array[a->used++] = element;
+}
+
+void insertpArray(pArray *a, struct patient element)
 {
     if(a->used == a->size)
     {
@@ -38,34 +61,94 @@ void insertArray(Array *a, struct patient element)
     }
     a->array[a->used++] = element;
 }
-void freeArray(Array *a)
+
+void freeArray(pArray *a)
 {
     free(a->array);
     a->array = NULL;
     a->size = a->used = 0;
 }
+void freeOpArray(opArray *a)
+{
+    free(a->array);
+    a->size = a->used = 0;
+}
+
+
+char* userInput()
+{
+	char* input = malloc(50);
+	fgets(input,50, stdin);
+	input[strcspn(input, "\n")] = 0; // remove newline
+	return input;
+};
+
 int main(int argc,char* argv[])
 {
-    Array patients;
-    initArray(&patients, 10);
-    
-    int count = 0;
-    printf("if you want to quit press(q): ");
-    while (true) {
-	
-	char input[100];
+    pArray patients;
+    initpArray(&patients, 10);
+    printf("if you want to quit press(q): \n");
+    bool shouldQuit = false;
+    while (!shouldQuit) {
+	char *input;
+	// adding patient 
 	printf("write patient name: \n");
-	// Using scanf (not safe for spaces):
-	scanf("%s", input);
-	if(strcmp(input, "q") == 0)break;
 	struct patient p;
-	strcpy(p.name, input);
-	insertArray(&patients, p);
-	count++;
+	initOpArray(&p.ops, 2);
+	input = userInput();
+	if(strcmp(input, "q") == 0)
+	{
+	    free(input);
+	    shouldQuit = true;
+	    break;
+	};
+	strcpy(p.name,input);
+	free(input);
+	//---------adding operations-------------
+	printf("types of operations: \n");
+	printf("type (p) to stop adding operations and return to patients: \n");
+	while(true){
+	    input = userInput();
+	    if (strcmp(input, "q") == 0) {
+		free(input);
+		shouldQuit = true;
+		break;
+	    }
+	    if(strcmp(input, "p") == 0)
+	    {
+		free(input);
+		break;
+	    };
+	    op new_op;
+	    strcpy(new_op.name,input);
+	    insertOpArray(&p.ops,new_op);
+	    free(input);
+	    //----------------------
+	}
+
+	insertpArray(&patients, p);
+	if (shouldQuit) {
+        break;
+    }
     }
     // Print the values of the array of structs
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < patients.used; i++) {
         printf("Patient %d: %s\n", i + 1, patients.array[i].name);
+
+	for (int r = 0; r < patients.array[i].ops.used; r++) {
+	    printf("operations performed: %d: %s\n",r+1,patients.array[i].ops.array[r].name);
+	}
     }
+
+	/*   FILE *fpt;*/
+	/*   fpt = fopen("data.csv", "a+");*/
+	/*   for(int i =0; i<patients.used;i++){*/
+	/*fprintf(fpt,"%d, %s,%s\n", i + 1, patients.array[i].name);*/
+	/*   }*/
+	/*   fclose(fpt);*/
+    for (int i = 0; i < patients.used; i++) {
+	freeOpArray(&patients.array[i].ops);
+}
+    freeArray(&patients);
     return 0;
 }
